c56648d944526070de31c9eb57888e3ea1f976f5
zyzdai <73833115+zyzdai@users.noreply.github.com>
2024-10-17 17:12:54 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -30,8 +30,9 @@ def go_edge_tts():
 
 @app.route('/edge_tts_post', methods=['POST'])  # Changed to POST
 def go_edge_tts_post():
+    print(request)
     data = request.json  # Expecting JSON data in the POST request
-    print(data)
+    
     text = data.get('text')
     voice = data.get('voice')
     rate = data.get('rate')
