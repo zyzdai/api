cbac5827219f9e3c283bd390d2b13a9902284643
zyzdai <73833115+zyzdai@users.noreply.github.com>
2024-12-15 03:02:50 +0800

    Add files via upload

---
+++ q.js
@@ -0,0 +1,10 @@
+#!name=番茄小说解析
+#!desc=番茄小说书源解析
+#!author=@Wuang备份
+#!openUrl=https://raw.githubusercontent.com/zyzdai/api/refs/heads/main/fq.plugin
+#!homepage=https://github.com/W126-L
+#!icon=https://raw.githubusercontent.com/W126-L/Tool/main/IconSet/fanqie.png
+#!tag=阿旺の库
+
+[Script]
+http-response ^https://reading.snssdk.com/reading/reader/full/v/\?item_id=* script-path=fq.js, requires-body=true, timeout=60
